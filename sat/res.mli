6cc3510d0efde3a2c033cab60b18bb25514d06e7
Simon Cruanes <simon.cruanes.2007@m4x.org>
2014-11-04 17:59:58 +0100

    copyright header in .header; authors in opam file

--- sat/res.mli
+++ sat/res.mli
@@ -1,4 +1,8 @@
-(* Copyright 2014 Guillaume Bury *)
+(*
+MSAT is free software, using the Apache license, see file LICENSE
+Copyright 2014 Guillaume Bury
+Copyright 2014 Simon Cruanes
+*)
 
 module type S = Res_intf.S
 
