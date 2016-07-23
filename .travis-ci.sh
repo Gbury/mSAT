13effa89693c0969b363f92403ca12133486cd02
Guillaume Bury <guillaume.bury@gmail.com>
2016-07-23 13:50:13 +0200

    Cleaner way to install opam in travis CI

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,16 +1,15 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS="ocamlfind ocamlbuild"
 
-echo "yes" | sudo add-apt-repository ppa:avsm/ocaml42+opam12
-sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
 export OPAMVERBOSE=1
+
 opam init
 opam switch ${OCAML_VERSION}
 eval `opam config env`
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
+
 make lib
 make bin
 make test
