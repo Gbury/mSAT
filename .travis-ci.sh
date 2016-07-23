0a4b8129240d99637310ee9527899f166df3523d
Guillaume Bury <guillaume.bury@gmail.com>
2016-07-23 13:58:45 +0200

    Moved more commands in before_install for travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,14 +1,7 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS="ocamlfind ocamlbuild"
 
-export OPAMYES=1
-export OPAMVERBOSE=1
-
-opam init
-opam switch ${OCAML_VERSION}
-eval `opam config env`
 opam install ${OPAM_DEPENDS}
-eval `opam config env`
 
 make lib
 make bin
