32128749b21720a0aa5da11c3f700bc023641b71
Guillaume Bury <guillaume.bury@gmail.com>
2016-12-02 15:57:56 +0100

    Some mli doc updates

--- src/core/formula_intf.ml
+++ src/core/formula_intf.ml
@@ -39,13 +39,15 @@ module type S = sig
   (** Formula constant. A valid formula should never be physically equal to [dummy] *)
 
   val neg : t -> t
-  (** Formula negation *)
+  (** Formula negation. Should be an involution, i.e. [equal a (neg neg a)] should
+      always hold. *)
 
   val norm : t -> t * negated
   (** Returns a 'normalized' form of the formula, possibly negated
-      (in which case return [Negated]).
-      [norm] must be so that [a] and [neg a] normalise to the same formula,
-      but one returns [Same_sign] and one returns [Negated] *)
+      (in which case return [Negated]). This function is used to recognize
+      the link between a formula [a] and its negation [neg a], so the goal is
+      that [a] and [neg a] normalise to the same formula,
+      but one returns [Same_sign] and the other one returns [Negated] *)
 
 end
 
