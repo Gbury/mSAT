c0d695dba329d4fc0256ae659384bb2696911362
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-01-18 19:42:41 -0600

    style: remove old headers

--- src/core/Heap.ml
+++ src/core/Heap.ml
@@ -1,15 +1,3 @@
-(**************************************************************************)
-(*                                                                        *)
-(*                                  Cubicle                               *)
-(*             Combining model checking algorithms and SMT solvers        *)
-(*                                                                        *)
-(*                  Mohamed Iguernelala                                   *)
-(*                  Universite Paris-Sud 11                               *)
-(*                                                                        *)
-(*  Copyright 2011. This file is distributed under the terms of the       *)
-(*  Apache Software License version 2.0                                   *)
-(*                                                                        *)
-(**************************************************************************)
 
 module type RANKED = Heap_intf.RANKED
 
