403026ba67fe739762dc040788d0aaf16bda5f65
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-02-11 08:44:15 -0600

    Revert "fix: catch `E_unsat` in assume, if one adds an empty clause"
    This reverts commit 5d7e34584bdbfd8326fbbf7f3314d93ac79597ce.

--- src/core/Internal.ml
+++ src/core/Internal.ml
@@ -1988,19 +1988,13 @@ module Make(Plugin : PLUGIN)
     with E_sat -> ()
 
   let assume st cnf lemma =
-    try
-      List.iter
-        (fun l ->
-           let atoms = List.rev_map (mk_atom st) l in
-           let c = Clause.make_permanent atoms (Hyp lemma) in
-           Log.debugf debug (fun k -> k "(@[sat.assume-clause@ @[<hov 2>%a@]@])" Clause.debug c);
-           Vec.push st.clauses_to_add c)
-        cnf
-    with
-    | E_unsat (US_false c) ->
-      st.unsat_at_0 <- Some c
-    | E_unsat (US_local _) ->
-      assert false (* assumptions should only be present in [solve] *)
+    List.iter
+      (fun l ->
+         let atoms = List.rev_map (mk_atom st) l in
+         let c = Clause.make_permanent atoms (Hyp lemma) in
+         Log.debugf debug (fun k -> k "(@[sat.assume-clause@ @[<hov 2>%a@]@])" Clause.debug c);
+         Vec.push st.clauses_to_add c)
+      cnf
 
   (* Check satisfiability *)
   let check_clause c =
